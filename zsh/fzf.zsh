b10185b0466ac9bd7066423d674c3d08c9f0c735
Eric DeMattos <38763305+edemattos@users.noreply.github.com>
2023-10-01 22:17:42 +0100

    fix typo in `fzf.zsh`

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,4 +1,4 @@
-# see ~/${XDG_CONFIG_HOME}/fd/ignore for excluded patterns
+# see ${XDG_CONFIG_HOME}/fd/ignore for excluded patterns
 
 [[ -f ${XDG_CONFIG_HOME}/fzf/fzf.zsh ]] && source ${XDG_CONFIG_HOME}/fzf/fzf.zsh
 
