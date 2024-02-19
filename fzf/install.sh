7be8eccf479d94ddfbd82423374dded5294eda90
Eric DeMattos <38763305+edemattos@users.noreply.github.com>
2024-02-19 23:32:18 +0000

    preserve keybindings, etc.

--- fzf/install.sh
+++ fzf/install.sh
@@ -11,4 +11,3 @@ echo "Installing fzf"
 git clone git@github.com:junegunn/fzf.git ${XDG_LOCAL_HOME}/fzf
 ${XDG_LOCAL_HOME}/fzf/install --bin --no-update-rc
 mv ${XDG_LOCAL_HOME}/fzf/bin/fzf ${XDG_LOCAL_HOME}/fzf/bin/fzf-tmux ${XDG_LOCAL_HOME}/bin/
-rm -rf ${XDG_LOCAL_HOME}/fzf
